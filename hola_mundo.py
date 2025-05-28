ecb15d3bb2669632aa914a5422960efd7f727c71
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-05-27 22:56:11 -0600

    Variables

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,1 +1,41 @@
 print("Hola Mundo")
+
+
+# Variables
+age = 18      # age is of type int
+print(age)
+
+name = "John" # name is now of type str
+print(name)
+
+float__number = 3.14159
+print(float__number)
+
+boolean = True
+print(boolean)
+
+dictionary = {"name": "John", "age": 18}
+print(dictionary)
+print(dictionary["name"])
+print(dictionary["age"])
+
+list__of__numbers = [1, 2, 3, 4, 5]
+print(list__of__numbers)
+
+tuple__of__numbers = (1, 2, 3, 4, 5)
+print(tuple__of__numbers)
+
+range__of__numbers = range(1, 10)
+print(range__of__numbers)
+
+''' 
+TIPOS DE DATOS:
+str	Text
+int, float, complex	Numeric
+list, tuple, range	Sequence
+dict	Mapping
+set, frozenset	Set
+bool	Boolean
+bytes, bytearray, memoryview	Binary
+'''
+
